388853ba21ed68a006d13c8f5be480a8c9484dd8
Gavin McNutt <gavinmcnutt1@gmail.com>
2023-02-03 23:18:47 -0800

    Added code to contribute functionality

---
+++ Develop/README.md
@@ -0,0 +1,15 @@
+# Facebook
+## Description
+A social media website for all people to interact
+## Install
+You just need a web browsers
+## Usage
+You login to your account and you find your friends by using their real name
+## License
+MIT License
+## Contributing
+You could make sure that nobody has an alt account to scam people
+## Tests
+You could make sure that the front end and the back end talk to each other effectively
+## Questions
+If you have any questions feel free to message me at gavinrn or gavinmcnutt1@gmail.com
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,95 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: "What is the title of your project?"
+    },
+    {
+        type: 'input',
+        name: 'desc',
+        message: "What is the description of your project?"
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: "How would you install your project?"
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: "How would you use your project?"
+    },
+    {
+        type: 'list',
+        name: 'License',
+        message: "What license is your project using?",
+        choices: ["Apache License 2.0" , "GNU GENERAL PUBLIC LICENSE v3.0" , "MIT License" , 'BSD 2-Clause "Simplified" License' ,
+            'BSD 3-Clause "New or "Revised" License' , 'Boost Software License - Version 1.0' , 'CC0 1.0 Universal' , 'Eclipse Public License - v 2.0',
+         'GNU AFFERO GENERAL PUBLIC LICENSE Version 3' , 'GNU GENERAL PUBLIC LICENSE Version 2', 'GNU LESSER GENERAL PUBLIC LICENSE Version 2.1',
+         'Mozilla Public License 2.0' , 'The Unlicense']
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: "How would you like others to contribute to the project?"
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: "How would you like others to write tests for you?"
+    },
+    {
+        type: 'input',
+        name: 'Github',
+        message: "What is your github?"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is your email"
+    },
+
+
+];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err)
+            throw err;
+        else
+           console.log("The file is updated with the given data");
+    }
+)}
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+
+        writeToFile("README.md" ,
+            `# ${answers.title}
+## Description
+${answers.desc}
+## Install
+${answers.install}
+## Usage
+${answers.usage}
+## License
+${answers.License}
+## Contributing
+${answers.contributing}
+## Tests
+${answers.tests}
+## Questions
+If you have any questions feel free to message me at ${answers.Github} or ${answers.email}`
+            );
+    }
+    );
+}
 
 // Function call to initialize app
 init();
